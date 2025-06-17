5e8d477f511f94733fb01f4a46b5d8c666fb6dfa
0xatanda <atanda0x@gmail.com>
2025-06-17 15:12:19 +0100

    Typing functional components

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {ButtonProps} from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({title, styles}) => {
+    return (
+        <div className={styles}>{title}</div>
+    )
+}
+
+export default Button;
